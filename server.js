530db5359aee494eb01a4d9e56fdc1cdd65300bc
Muhammad Asim <mrgoblin@Muhammads-MacBook-Pro.local>
2023-08-03 00:03:28 +0500

    basic redis server ready with ejs template

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ const app = express()
 
 // setting redis client
 const redisClient = redis.createClient()
- redisClient.connect()
-redisClient.on('error', err => console.log('Redis Client Error', err));
+redisClient.connect()
+redisClient.on('error', (err) => console.log('Redis Client Error', err))
 redisClient.on('connect', () => console.log('redis Server connected'))
 
 // setting view engine
@@ -18,17 +18,22 @@ app.set('view engine', 'ejs')
 
 app.use(morgan('dev'))
 app.use(express.json())
-app.use(express.urlencoded({ extended: false}))
+app.use(express.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', getTasks)
 
 async function getTasks(req, res, next) {
-    const tasks = await redisClient.lRange('tasks', 0, -1, )
-    res.send(tasks);
+  const title = 'Task List'
+  const tasks = await redisClient.lRange('tasks', 0, -1)
+  if (tasks.length > 0) {
+    res.render('index', {
+      title,
+      tasks,
+    })
+  }
 }
 
 app.listen(3000, () => {
-    console.log('server is listening on http://localhost:3000')
+  console.log('server is listening on http://localhost:3000')
 })
-
